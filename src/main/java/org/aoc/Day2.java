46464cfece34dcaf3ff1d0bbfb9b4f17df830f9e
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-12-02 14:17:23 -0500

    Day 2, part 2

--- src/main/java/org/aoc/Day2.java
+++ src/main/java/org/aoc/Day2.java
@@ -15,16 +15,40 @@ public class Day2 {
                 "green", 13,
                 "blue", 14
         );
-        return input.lines().map(line -> compute(line, avalableCubes)).reduce(0, Integer::sum);
+        return input.lines()
+                .map(line -> computePossible(line, avalableCubes))
+                .reduce(0, Integer::sum);
     }
 
-    public static Integer compute(String line, Map<String, Integer> availableCubes) {
+    public static int day2p2(String input) {
+        return input.lines().map(Day2::computeFewest).reduce(0, Integer::sum);
+    }
+
+    private static Integer computeFewest(String line) {
+        String[] game = line.split(":");
+        var fewest = buildFewest(game[1].trim());
+        return fewest.values().stream().reduce(1, (x, y) -> x * y);
+    }
+
+    // line: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    private static Integer computePossible(String line, Map<String, Integer> availableCubes) {
         String[] game = line.split(":");
         int id = Integer.parseInt(game[0].split(" ")[1]);
         boolean isPossible = isGamePossible(game[1].trim(), availableCubes);
         return isPossible ? id : 0;
     }
 
+    // game: "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    private static Map<String, Integer> buildFewest(String game) {
+        List<Turn> turns = buildTurns(game).stream().flatMap(List::stream).toList();
+        Map<String, Integer> fewest = new HashMap<>();
+        for (Turn turn: turns) {
+            fewest.merge(turn.name, turn.val, Integer::max);
+        }
+        return fewest;
+    }
+
+    // game: "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
     private static boolean isGamePossible(String game, Map<String, Integer> availableCubes) {
         List<List<Turn>> turns = buildTurns(game);
         return turns.stream().noneMatch(turn -> notEnoughCubes(turn, availableCubes));
@@ -54,14 +78,6 @@ public class Day2 {
                 .map(t -> t.split(" "))
                 .map(t -> new Turn(t[1], Integer.valueOf(t[0])))
                 .collect(Collectors.toList());
-    }
-
-    private static Map<String, Integer> getCubes(List<Turn> turns) {
-        Map<String, Integer> cubes = new HashMap<>();
-        for (Turn turn: turns) {
-            cubes.merge(turn.name, turn.val, Integer::sum);
-        }
-        return cubes;
     }
 
     record Turn(String name, Integer val) {}
--- src/test/java/org/aoc/Day2Test.java
+++ src/test/java/org/aoc/Day2Test.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day2Test implements TestResources {
 
     @Test
-    public void day1p1Simple() {
+    public void day2p1Simple() {
         String input = """
 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -20,9 +20,29 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     }
 
     @Test
-    public void day1p1() {
+    public void day2p1() {
         String input = loadResource("day2.txt");
         int ans = Day2.day2p1(input);
-        assertEquals(8, ans);
+        assertEquals(3099, ans);
+    }
+
+    @Test
+    public void day2p2Simple() {
+        String input = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+""";
+        int ans = Day2.day2p2(input);
+        assertEquals(2286, ans);
+    }
+
+    @Test
+    public void day2p2() {
+        String input = loadResource("day2.txt");
+        int ans = Day2.day2p2(input);
+        assertEquals(72970, ans);
     }
 }
