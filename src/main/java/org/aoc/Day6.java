33a5b9a94b9ce181381f6a6a58f6998839a14c0d
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-12-09 10:17:31 -0500

    Day 6, part 2

--- src/main/java/org/aoc/Day6.java
+++ src/main/java/org/aoc/Day6.java
@@ -5,19 +5,26 @@ import java.util.List;
 
 public class Day6 {
 
-    public static int part1(List<Integer[]> races) {
-        return races.stream().map(Day6::processOne).reduce(1, (x, y) -> x * y);
+    public static long part1(List<Integer[]> races) {
+        return races.stream()
+                .map(x -> new Long[] {(long) x[0], (long) x[1]})
+                .map(Day6::processOne)
+                .reduce(1L, (x, y) -> x * y);
     }
 
-    public static int processOne(Integer[] race) {
-        int end = race[0];
-        int time = race[1];
+    public static long part2(List<Long[]> races) {
+        return races.stream().map(Day6::processOne).reduce(1L, (x, y) -> x * y);
+    }
+
+    public static long processOne(Long[] race) {
+        long end = race[0];
+        long time = race[1];
 
         List<Integer> ans = new ArrayList<>();
 
         for (int i = 1; i <= end; ++i) {
-            int moving = (end - i);
-            int diff = (moving * i);
+            long moving = (end - i);
+            long diff = (moving * i);
             if (time < diff) {
                 ans.add(i);
             }
--- src/test/java/org/aoc/Day6Test.java
+++ src/test/java/org/aoc/Day6Test.java
@@ -2,6 +2,7 @@ package org.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,12 +17,12 @@ class Day6Test {
                 new Integer[]{30, 200}
         );
 
-        int ans = Day6.part1(input);
+        long ans = Day6.part1(input);
         assertEquals(288, ans);
     }
 
     @Test
-    public void part2() {
+    public void part1() {
         List<Integer[]> input = List.of(
                 new Integer[]{50, 242},
                 new Integer[]{74, 1017},
@@ -29,7 +30,17 @@ class Day6Test {
                 new Integer[]{85, 1252}
         );
 
-        int ans = Day6.part1(input);
+        long ans = Day6.part1(input);
         assertEquals(1731600, ans);
+    }
+
+    @Test
+    public void part2() {
+        List<Long[]> input = Collections.singletonList(
+                new Long[]{50748685L, 242101716911252L}
+        );
+
+        long ans = Day6.part2(input);
+        assertEquals(40087680, ans);
     }
 }
