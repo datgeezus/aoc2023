8750ad9ed51a5ed805ca2bd54a1fcfefe9bfb6cd
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-12-09 15:21:06 -0500

    Day 8, part 2 (not working)

--- src/main/java/org/aoc/Day8.java
+++ src/main/java/org/aoc/Day8.java
@@ -1,8 +1,7 @@
 package org.aoc;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Day8 {
@@ -15,12 +14,67 @@ public class Day8 {
 
     public static int part1(String input) {
         Deque<Step> steps = buildSteps(input.lines().findFirst().get());
+        Map<String, String[]> graph = buildGraph(input);
+        return nSteps(graph, steps, START, END::equals);
+    }
 
-        Map<String, String[]> graph =
-                input.lines().skip(2)
-                        .map(Day8::buildNode)
-                        .collect(Collectors.toMap(node -> node.key, node -> node.children));
-        return nSteps(graph, steps);
+    public static int part2(String input) {
+        Deque<Step> steps = buildSteps(input.lines().findFirst().get());
+        Map<String, String[]> graph = buildGraph(input);
+        List<String> starts = graph.keySet().stream().filter(k -> k.endsWith("A")).toList();
+
+        return starts.stream()
+                .map(start -> nSteps(graph, steps, start, (s) -> s.endsWith("Z")))
+                .reduce(1, (a, b) -> a * b);
+
+//        return nSteps(graph, steps, starts, Day8::atEnd);
+    }
+
+    private static boolean atEnd(List<String> now) {
+        return now.stream().allMatch(s -> s.endsWith("Z"));
+    }
+
+    private static int nSteps(
+            Map<String, String[]> graph, Deque<Step> steps, String start, Predicate<String> atEnd) {
+        String now = start;
+        int n = 0;
+        while (!atEnd.test(now)) {
+            Step step = steps.pop();
+            String[] children = graph.get(now);
+            now = children[step.ordinal()];
+            steps.add(step);
+            n += 1;
+        }
+
+        return n;
+    }
+
+    private static int nSteps(
+            Map<String, String[]> graph,
+            Deque<Step> steps,
+            List<String> starts,
+            Predicate<List<String>> atEnd) {
+
+        List<String> now = starts;
+        int n = 0;
+        while (!atEnd.test(now)) {
+            Step step = steps.pop();
+            List<String[]> children = now.stream().map(graph::get).toList();
+
+            now = children.stream().map(c -> c[step.ordinal()]).collect(Collectors.toList());
+
+            steps.add(step);
+            n += 1;
+        }
+
+        return n;
+    }
+
+    private static Map<String, String[]> buildGraph(String lines) {
+        return lines.lines()
+                .skip(2)
+                .map(Day8::buildNode)
+                .collect(Collectors.toMap(node -> node.key, node -> node.children));
     }
 
     private static Deque<Step> buildSteps(String line) {
@@ -38,19 +92,5 @@ public class Day8 {
         String key = parts[0].trim();
         String[] children = parts[1].substring(2, parts[1].length() - 1).split(",\\s+");
         return new Node(key, children);
-    }
-
-    private static int nSteps(Map<String, String[]> graph, Deque<Step> steps) {
-        String now = START;
-        int n = 0;
-        while (!END.equals(now)) {
-            Step step = steps.pop();
-            String[] children = graph.get(now);
-            now = children[step.ordinal()];
-            steps.add(step);
-            n += 1;
-        }
-
-        return n;
     }
 }
--- src/test/java/org/aoc/Day8Test.java
+++ src/test/java/org/aoc/Day8Test.java
@@ -26,6 +26,19 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 """;
 
+    private static final String INPUT3 = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+""";
+
     @Test
     public void part1Simple() {
         int ans = Day8.part1(INPUT);
@@ -42,6 +55,19 @@ ZZZ = (ZZZ, ZZZ)
     public void part1() {
         String input = loadResource("day8.txt");
         int ans = Day8.part1(input);
+        assertEquals(17287, ans);
+    }
+
+    @Test
+    public void part2Simple() {
+        int ans = Day8.part2(INPUT3);
+        assertEquals(6, ans);
+    }
+
+    @Test
+    public void part2() {
+        String input = loadResource("day8.txt");
+        int ans = Day8.part2(input);
         assertEquals(17287, ans);
     }
 }
