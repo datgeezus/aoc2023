ee49e6204a11e52548219db3961b93cda03c5756
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-12-12 22:00:46 -0500

    Day 9, part 1 (not working)

---
+++ src/main/java/org/aoc/Day9.java
@@ -0,0 +1,22 @@
+package org.aoc;
+
+import java.util.Arrays;
+
+public class Day9 {
+
+    public static int part1(String input) {
+        return input.lines()
+                .map(line -> line.split("\\s+"))
+                .map(Day9::toNums)
+                .map(Day9::processOne)
+                .reduce(0, Integer::sum);
+    }
+
+    private static int processOne(Integer[] history) {
+
+    }
+
+    private static Integer[] toNums(String[] history) {
+        return Arrays.stream(history).map(Integer::parseInt).toArray(Integer[]::new);
+    }
+}
